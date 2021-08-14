1bfaba963baa7ed4dd0aa92f15b6cc9785502da8
Jam Nguyen <jam@jamnguyen.com>
2021-08-14 15:31:27 +0700

    update config.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './styles/global/index.scss';
+import './styles/index.scss';
 import App from './App';
 
 render(<App />, document.getElementById('root'));
